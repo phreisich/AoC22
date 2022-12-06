0b13ebddcdd18f5e874615b3e6fda9baba32ceb4
phreisich <37024571+phreisich@users.noreply.github.com>
2022-12-06 16:28:39 +0100

    Add files via upload

---
+++ Day6/day6.py
@@ -0,0 +1,12 @@
+input = open("input.txt", "r").read()
+
+def get_marker_pos(num_unique):
+    tmp = ""
+    marker_found = False
+    for i in input:
+        if (marker_found == False):
+            tmp += i
+            marker_found = len(set(tmp[-num_unique:])) == num_unique
+    return(len(tmp))
+
+print(get_marker_pos(14))
